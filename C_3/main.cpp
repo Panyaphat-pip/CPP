193fd8c49a8038bd3c366a91472aa0712f685a66
Panyaphat-pip <pxnyaphat.pip@gmail.com>
2023-02-16 15:36:38 +0700

    update

--- C_3/main.cpp
+++ C_3/main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-int wide , length, high ;
+int wide , length ;
 float sum ;
 int main() {
     printf("Enter wide ");
     scanf("%d", &wide);
     printf("Enter length");
     scanf("%d", &length);
-    printf("Enter high");
-    scanf("%d", &high);
-    sum = wide*length*high;
-    printf("Wide = %d \nLength = %d \nHigh = %d\nThe result of Rectangle is %f ", wide, length, high, sum);
+    sum = wide*length;
+    printf("Wide = %d \nLength = %d\nThe result of Rectangle is %f ", wide, length, sum);
 
 
 
